637b6e28830a91bc25f08aedd257f3b60d90e850
Filipa Merino <filipaam@outlook.com>
2023-03-15 17:21:04 +0000

    display weather icon

--- app.js
+++ app.js
@@ -7,26 +7,36 @@ const app = express();
 
 app.get("/", function(req, res){
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=1233736454d5aaae70c67fad70b09f02"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=1233736454d5aaae70c67fad70b09f02&units	=metric"
 
   https.get(url, function(response){
-    console.log(response.statusCode);
 
     response.on("data", function(data){
 
       const weatherInfo = JSON.parse(data);
 
-      const temp = weatherInfo.main.temp
+      const temp = weatherInfo.main.temp;
       console.log(temp);
 
-      const humidity = weatherInfo.main.humidity
-      console.log(humidity);
+      const country = weatherInfo.sys.country;
+      console.log(country);
+
+      const description = weatherInfo.weather[0].description;
+      console.log(description);
+
+
+      const icon = weatherInfo.weather[0].icon;
+      const iconURL = "https://openweathermap.org/img/wn/" + icon +"@2x.png";
+
+      res.write("<p> In Lisbon(" + country + "), the weather is currently " + description + ".</p>");
+      res.write("<h1>The temperature is " + temp + " degrees celsius.</h1>");
+      res.write("<img src=" + iconURL + ">");
+      res.send();
     })
 
   });
 
 
-  res.send("You're in!")
 });
 
 app.listen(3000);
