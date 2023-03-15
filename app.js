e56d790996b8f1577c2a6341adcabd9b197f96b4
Filipa Merino <filipaam@outlook.com>
2023-03-15 16:43:24 +0000

    use weather API

--- app.js
+++ app.js
@@ -10,7 +10,19 @@ app.get("/", function(req, res){
   const url = "https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=1233736454d5aaae70c67fad70b09f02"
 
   https.get(url, function(response){
-    console.log(response);
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+
+      const weatherInfo = JSON.parse(data);
+
+      const temp = weatherInfo.main.temp
+      console.log(temp);
+
+      const humidity = weatherInfo.main.humidity
+      console.log(humidity);
+    })
+
   });
 
 
